615d91c41392383d75f794620c7812220e7719d1
Gabriel Bursi <gabriellbursii@gmail.com>
2023-04-20 21:08:42 -0300

    config do mapa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import { Main } from "components";
+import dynamic from "next/dynamic";
+
+const DynamicComponent = dynamic(() =>
+  import('../components/Map').then((mod) => mod.MapComponent)
+, { ssr: false })
 
 export default function Home() {
-  return <Main />
+  return <DynamicComponent />
 }
